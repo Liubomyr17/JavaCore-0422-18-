21e495e027baec24ca9c3a90ae74b6182dd53cee
Code First! <41821541+Liubomyr17@users.noreply.github.com>
2020-01-31 20:46:51 +0200

    Add files via upload

---
+++ JavaCore 0422 18+/src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+      BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+    String name = reader.readLine();
+    int age = Integer.parseInt(reader.readLine());
+    if (age < 18)
+        System.out.println("SMALL");
+    }
+   }
